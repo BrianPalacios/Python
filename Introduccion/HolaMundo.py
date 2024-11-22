cb7916c35fe6bafa0ace843d77f3381bf56a3da2
loganbbt142@gmail.com <loganbbt142@gmail.com>
2024-11-22 16:42:49 -0500

    primer commit

---
+++ Introduccion/HolaMundo.py
@@ -0,0 +1,22 @@
+print('Hola mundo')
+print(type(10))
+
+#Operadores
+print('Suma = ',5+6)
+print('Resta = ',6-2)
+print('Multiplicion = ', 6*2)
+print('Division = ', 6/2)
+print('Exponencial = ', 6**2)
+print('Modulo = ', 7%2)
+print('Cociente = ', 7%2)
+
+#Tipo de datos
+print('10 es un tipo de dato = ', type(10))
+print('9.8 es un tipo de dato = ', type(9.8))
+print('3.14 es un tipo de dato = ', type(3.14))
+print('(4-4j) es un tipo de dato = ', type(4-4j))
+print('[Asabeneh, Python, Finlandia] es un tipo de dato = ', type(['Asabeneh', 'Python', 'Finlandia']))
+print('Brian es un tipo de dato = ', type('Brian'))
+
+#Distancia Euclidiana
+print('La distancia euclidiana de (2,3) y (10,8) es ', ((10-2)**2+(8-3)**2)** 0.5)
